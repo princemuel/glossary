4f631676d514bb2804bdaa59a8c6b3cd94d51e61
pHoeniX-svg <vansomecsam@gmail.com>
2022-02-14 19:31:30 +0100

    setup index.html file and added project start script

---
+++ webpack/webpack.config.js
@@ -0,0 +1,29 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+  entry: path.resolve(__dirname, '..', './src/index.tsx'),
+  resolve: {
+    extensions: ['.tsx', '.ts', '.jsx', '.js'],
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(ts|js)x?$/,
+        exclude: /node_modules/,
+        use: [{ loader: 'babel-loader' }],
+      },
+    ],
+  },
+  output: {
+    path: path.resolve(__dirname, '..', './build'),
+    filename: 'bundle.js',
+  },
+  mode: 'development',
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: path.resolve(__dirname, '..', './index.html'),
+    }),
+  ],
+  stats: 'errors-only',
+};
