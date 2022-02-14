1ad8ca9ac43ba35ddc22aec0271008d7880b20d5
pHoeniX-svg <vansomecsam@gmail.com>
2022-02-14 19:51:28 +0100

    ddaed image config

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,15 @@
+import FacebookIcon from './facebook.svg';
+import './index.css';
+import InstagramIcon from './instagram.png';
+
 const App = () => {
-  return <h1>React Typescript Webpack Starter Template</h1>;
+  return (
+    <div>
+      <h1>React Typescript Webpack Starter Template</h1>
+      <img src={FacebookIcon} alt='image' />
+      <img src={InstagramIcon} alt='image' />
+    </div>
+  );
 };
 
 export { App };
--- webpack/webpack.common.js
+++ webpack/webpack.common.js
@@ -1,5 +1,6 @@
 const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 
 module.exports = {
   entry: path.resolve(__dirname, '..', './src/index.tsx'),
@@ -13,14 +14,36 @@ module.exports = {
         exclude: /node_modules/,
         use: [{ loader: 'babel-loader' }],
       },
+      {
+        test: /\.(s[ac]|c)ss$/i,
+        use: [
+          {
+            loader: MiniCssExtractPlugin.loader,
+            options: {
+              publicPath: '',
+            },
+          },
+          'css-loader',
+          'postcss-loader',
+          'sass-loader',
+        ],
+      },
+      {
+        test: /\.(?:ico|png|jpe?g|gif|webp)$/i,
+        type: 'asset/resource',
+      },
+      {
+        test: /\.(woff(2)?|eot|ttf|otf|svg|)$/,
+        type: 'asset/inline',
+      },
     ],
   },
   output: {
     path: path.resolve(__dirname, '..', './build'),
     filename: 'bundle.js',
   },
-  mode: 'development',
   plugins: [
+    new MiniCssExtractPlugin(),
     new HtmlWebpackPlugin({
       template: path.resolve(__dirname, '..', './index.html'),
     }),
