9a8957db7f8d80c8c1a73397bb15b252b34e97a7
Prince Muel <68931805+princemuel@users.noreply.github.com>
2025-05-02 23:04:03 +0100

    ci: update git/editor settings

--- src/app.d.ts
+++ src/app.d.ts
@@ -1,5 +1,7 @@
 // See https://svelte.dev/docs/kit/types#app.d.ts
 // for information about these interfaces
+import '@poppanator/sveltekit-svg/dist/svg.d.ts';
+import 'unplugin-icons/types/svelte';
 declare global {
   namespace App {
     // interface Error {}
