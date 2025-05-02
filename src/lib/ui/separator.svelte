322ce3be2507f9fb612ebab60a0f9db224170dd6
Prince Muel <68931805+princemuel@users.noreply.github.com>
2025-05-02 23:02:47 +0100

    feat(ui): scaffold ui blocks

---
+++ src/lib/ui/separator.svelte
@@ -0,0 +1,17 @@
+<script lang="ts">
+  import { Separator as SeparatorPrimitive } from 'bits-ui';
+
+  type Props = SeparatorPrimitive.RootProps;
+  let { orientation = 'horizontal', decorative = true, ...attrs }: Props = $props();
+</script>
+
+<SeparatorPrimitive.Root
+  {...attrs}
+  {orientation}
+  {decorative}
+  class={[
+    'bg-border shrink-0',
+    orientation === 'horizontal' ? 'h-[1px] w-full' : 'min-h-full w-[1px]',
+    attrs.class
+  ]}
+/>
