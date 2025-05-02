e7109805b7b065eb5f3cdd2a0b51c620adf46c8d
Prince Muel <68931805+princemuel@users.noreply.github.com>
2025-05-02 22:57:56 +0100

    feat(media): add assets

---
+++ src/lib/icons/index.ts
@@ -0,0 +1,3 @@
+export { default as IconChevronDown } from './chevron-down.svg?component';
+export { default as IconLogo } from './logo.svg?component';
+export { default as IconMoon } from './moon.svg?component';
